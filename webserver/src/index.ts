216504e27925d82cf81e22e32e28471532b198bd
John Whitton <john@johnwhitton.com>
2021-08-15 19:41:14 -0700

    Add Kanga

--- webserver/src/index.ts
+++ webserver/src/index.ts
@@ -6,13 +6,48 @@ app.use(express.json());
 
 var bridge = require('../src/bridge.js');
 var viper = require('../src/viper.ts');
+var kanga = require('../src/kanga.ts');
 var cors=require('cors');
 
 app.use(cors({origin:true,credentials: true}));
 
+// Hello World
 app.get('/',(req, res) => {
     res.send('Hello World!');
 });
+
+
+
+// Swap Quote single Pool
+
+app.get('/swap/quote/get',(req, res) => {
+  viper.ExactInputTrade();
+  res.send('Here is your swap quote Get!');
+
+});
+
+app.post('/swap/quote', async (req, res) => {
+
+  //console.log(await viper.ExactInputTradeQuote())
+  let trade = await viper.ExactInputTradeQuote();
+  // res.send('Here is your swap quote!');
+  // console.log(trade)
+  res.json(trade);
+
+});
+
+// Swap Quote Multi Pool
+
+// Swap Execute Harmony Only
+
+// Liquidity Provide Harmony Only
+
+// Swap Execute using Bridge
+
+// Liquidity Provisioning using Bridge
+
+// Execute Swap Using Bridge 
+// 
 app.post('/lp/pair',(req, res) => {
 });
 app.post('/lp/explore',(req, res) => {
